3cd3125647cc5816ed84ade90fb343959f07a713
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2023-06-08 19:25:34 +0200

    Project structure and Users logic

---
+++ src/users/users.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    // We use delete if we don't have any hooks or any other logic to run. It's faster (1 query to database) --> delete(id)
+    // return this.repo.delete(id);
+    // If we have hooks or other logic to run, we use remove (2 queries to database, it's slower). --> remove(Entity)
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return this.repo.remove(user);
+  }
+}
